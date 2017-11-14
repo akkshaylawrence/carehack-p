eaf44856b55a28e47d946cd1ad08cbeeef1402ae
BoyWithSilverWings <agney.menons@live.com>
2017-11-14 21:57:28 +0530

    React Intiial + Router

---
+++ ui/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div />
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ ui/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+//Adding support for <IE11 because React 16 uses Map and Set. These are not natively supported on these browsers.
+import "core-js/es6/map";
+import "core-js/es6/set";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("/sw.js").then(
+      function(registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function(err) {
+        // Registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
