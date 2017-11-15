a4a681752a4982beb6b1aa85c1bff1485edc14c8
BoyWithSilverWings <agney.menons@live.com>
2017-11-15 23:55:51 +0530

    Changes

--- ui/src/Front/Book.jsx
+++ ui/src/Front/Book.jsx
@@ -2,16 +2,55 @@ import React from "react";
 import flatpickr from "flatpickr";
 import Flatpickr from "react-flatpickr";
 import uikit from "uikit";
-import app from "../js/app";
 import { Link } from "react-router-dom";
+import firebase from "firebase";
+require("firebase/firestore");
 import "flatpickr/dist/themes/material_green.css";
 
 class Book extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      date: new Date()
+      date: new Date(),
+      speciality: ""
     };
+    this.initializeFirebase();
+    this.initializeFirebase = this.initializeFirebase.bind(this);
+    this.handleSpeciality = this.handleSpeciality.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  initializeFirebase() {
+    const config = {
+      apiKey: "AIzaSyD2KHYE4_wQWeyPLKemMESWCxX0x0S7nFM",
+      authDomain: "findcare-5e020.firebaseapp.com",
+      databaseURL: "https://findcare-5e020.firebaseio.com",
+      projectId: "findcare-5e020",
+      storageBucket: "findcare-5e020.appspot.com",
+      messagingSenderId: "484085783680"
+    };
+    firebase.initializeApp(config);
+  }
+  handleSpeciality(event) {
+    this.setState(
+      {
+        speciality: event.target.value
+      },
+      () => console.log(this.state.speciality)
+    );
+    const db = firebase.Firestore();
+    db
+      .collection("hospital")
+      .get()
+      .then(res => console.log(res));
+  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
   render() {
     const { date } = this.state;
@@ -38,18 +77,36 @@ class Book extends React.Component {
             </div>
           </div>
           <div className="uk-margin">
-            <label className="uk-form-label" htmlFor="form-stacked-text">
+            <label className="uk-form-label" htmlFor="sectionSel">
               Select Department
             </label>
-            <select id="sectionSel" className="uk-select">
-              <option value="0">Select Department</option>
+            <select
+              id="sectionSel"
+              className="uk-select"
+              onChange={this.handleSpeciality}
+              name="speciality"
+            >
+              <option
+                value="0"
+                disabled
+                selected
+                className="placeholder-select"
+              >
+                Select Department
+              </option>
+              <option value="cardiology">Cardiology</option>
+              <option value="anesthesiologist">Anesthesiologist</option>
+              <option value="gastroenterology">Gastroenterology</option>
+              <option value="cardiovascular-surgeon">
+                Cardiovascular surgeon
+              </option>
             </select>
           </div>
           <div className="uk-margin">
-            <label className="uk-form-label" htmlFor="form-stacked-text">
+            <label className="uk-form-label" htmlFor="doctor">
               Select Doctor
             </label>
-            <select id="serviceSel" className="uk-select" />
+            <select id="doctorSel" className="uk-select" name="doctor" />
           </div>
           <div className="uk-margin-small">
             <label className="uk-form-label" htmlFor="form-stacked-text">
