81e945a3961b1f23db8398a1e105671782618a15
BoyWithSilverWings <agney.menons@live.com>
2017-11-15 23:01:05 +0530

    OTP Sending

--- ui/src/Front/Register.jsx
+++ ui/src/Front/Register.jsx
@@ -1,6 +1,88 @@
 import React from "react";
+import firebase from "firebase";
 
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      phone: "",
+      otp: "",
+      smsSent: false
+    };
+    this.initializeFirebase();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.onSignUpSubmit = this.onSignUpSubmit.bind(this);
+    this.getCodeFromUserInput = this.getCodeFromUserInput.bind(this);
+  }
+  initializeFirebase() {
+    const config = {
+      apiKey: "AIzaSyD2KHYE4_wQWeyPLKemMESWCxX0x0S7nFM",
+      authDomain: "findcare-5e020.firebaseapp.com",
+      databaseURL: "https://findcare-5e020.firebaseio.com",
+      projectId: "findcare-5e020",
+      storageBucket: "findcare-5e020.appspot.com",
+      messagingSenderId: "484085783680"
+    };
+    firebase.initializeApp(config);
+    firebase.auth().useDeviceLanguage();
+  }
+  componentDidMount() {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "sign-up-button",
+      {
+        size: "invisible",
+        callback: response => {
+          this.onSignUpSubmit();
+        },
+        "expired-callback": function() {
+          // Response expired. Ask user to solve reCAPTCHA again.
+          console.log("Expired");
+        }
+      }
+    );
+  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+  onSignUpSubmit(event) {
+    event && event.preventDefault();
+    const phoneNumber = "+91" + this.state.phone;
+    const appVerifier = window.recaptchaVerifier;
+    firebase
+      .auth()
+      .signInWithPhoneNumber(phoneNumber, appVerifier)
+      .then(confirmationResult => {
+        this.setState({
+          smsSent: true
+        });
+        window.confirmationResult = confirmationResult;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  getCodeFromUserInput(event) {
+    event.preventDefault();
+    const code = this.state.otp;
+    confirmationResult
+      .confirm(code)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        // ...
+      })
+      .catch(error => {
+        // User couldn't sign in (bad verification code?)
+        // ...
+      });
+  }
   render() {
     return (
       <div>
@@ -11,48 +93,87 @@ class Register extends React.Component {
         <hr className="uk-margin-small" />
         <form className="uk-form-stacked">
           <div className="uk-margin-small">
-            <label className="uk-form-label" htmlFor="form-stacked-text">
+            <label className="uk-form-label" htmlFor="name">
               Name
             </label>
             <div className="uk-inline uk-width-1-1">
               <span className="uk-form-icon" uk-icon="icon: user" />
-              <input id="name" className="uk-input" type="text" />
+              <input
+                id="name"
+                name="name"
+                required
+                className="uk-input"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleInputChange}
+              />
             </div>
           </div>
           <div id="phone" className="uk-margin-small">
-            <label className="uk-form-label" htmlFor="form-stacked-text">
+            <label className="uk-form-label" htmlFor="phone">
               Phone Number
             </label>
             <div className="uk-inline uk-width-1-1">
               <span className="uk-form-icon" uk-icon="icon: phone" />
-              <input id="phone" className="uk-input" type="text" />
+              <input
+                id="phone"
+                name="phone"
+                className="uk-input"
+                type="text"
+                required
+                value={this.state.phone}
+                onChange={this.handleInputChange}
+              />
             </div>
-            <label
+            {/* <label
               className="uk-form-label uk-text-right"
               htmlFor="form-stacked-text"
             >
               Enter OTP Below
-            </label>
+            </label> */}
           </div>
-          <div id="otp" className="uk-margin-small uk-margin-remove-top">
-            <label className="uk-form-label" htmlFor="form-stacked-text">
-              OTP
-            </label>
-            <div className="uk-inline uk-width-1-1">
-              <span className="uk-form-icon" uk-icon="icon: hashtag" />
-              <input className="uk-input" type="text" />
+          {this.state.smsSent ? (
+            <div id="otp" className="uk-margin-small uk-margin-remove-top">
+              <label className="uk-form-label" htmlFor="otp">
+                OTP
+              </label>
+              <div className="uk-inline uk-width-1-1">
+                <span className="uk-form-icon" uk-icon="icon: hashtag" />
+                <input
+                  id="otp"
+                  name="otp"
+                  className="uk-input"
+                  type="number"
+                  value={this.state.otp}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <label
+                className="uk-form-label uk-text-right"
+                htmlFor="form-stacked-text"
+              >
+                <a className="uk-link-reset" href="">
+                  Resend?
+                </a>
+              </label>
             </div>
-            <label
-              className="uk-form-label uk-text-right"
-              htmlFor="form-stacked-text"
-            >
-              <a className="uk-link-reset" href="">
-                Resend?
-              </a>
-            </label>
-          </div>
+          ) : (
+            <div className="uk-margin-small">
+              <button
+                className="uk-button reg uk-button-primary uk-width-1-1"
+                id="sign-up-button"
+                onClick={this.onSignUpSubmit}
+              >
+                Verify Number
+              </button>
+            </div>
+          )}
           <div className="uk-margin-small">
-            <button className="uk-button reg uk-button-primary uk-width-1-1">
+            <button
+              className="uk-button reg uk-button-primary uk-width-1-1"
+              disabled={!this.state.smsSent}
+              onClick={this.getCodeFromUserInput}
+            >
               Register
             </button>
           </div>
